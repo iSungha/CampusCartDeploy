408a897bce59e29854a9975a06ab184a3ffa310a
iSungha <007.garry.earl@gmail.com>
2026-06-27 19:05:07 -0300

    Allow Cors

--- backend/config/swagger.js
+++ backend/config/swagger.js
@@ -8,7 +8,7 @@ const swaggerDocument = {
   },
   servers: [
     {
-      url: "http://localhost:5000",
+      url: process.env.API_BASE_URL || "http://localhost:5000",
       description: "Local development server"
     }
   ],
